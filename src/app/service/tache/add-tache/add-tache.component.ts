38edeafd0297931d26f7f99c599cd6f682ff2102
haouari-mohamed <mohamedhaouari71@gmail.com>
2024-09-13 10:19:43 +0100

    j'ai ajoute showalltaches et deletetache et j'ai ajoute les endspoints dans service

---
+++ src/app/service/tache/add-tache/add-tache.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-tache',
+  templateUrl: './add-tache.component.html',
+  styleUrl: './add-tache.component.css'
+})
+export class AddTacheComponent {
+
+}
--- src/app/service/tache/tache.service.ts
+++ src/app/service/tache/tache.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Tache } from '../../model/Construction.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -7,5 +9,26 @@ import { Injectable } from '@angular/core';
 export class TacheService {
 
   constructor(private http:HttpClient) { }
+
+  urlApi="http://localhost:8888";
+
+  public showtache(id:number):Observable<Tache[]>{
+    return this.http.get<Tache[]>(`${this.urlApi}/TACHE-SERVICE/taches/${id}`)
+  }
+ 
+  public addTache(id:number,tache:Tache){
+    return this.http.post(`${this.urlApi}/TACHE-SERVICE/admin/taches/${id}`,tache)
+  }
+
+  public deleteTache(id:number){
+    return this.http.delete(`${this.urlApi}/TACHE-SERVICE/admin/taches/${id}`)
+  }
+  public updateTache(id:number,tache:Tache){
+    return this.http.put(`${this.urlApi}/TACHE-SERVICE/admin/taches/${id}`,tache)
+  }
+
   
+  public findById(id:number):Observable<Tache>{
+    return this.http.get<Tache>(`${this.urlApi}/TACHE-SERVICE/admin/taches/find/${id}`)
+  }
 }
